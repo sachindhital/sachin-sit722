0d74e8715795450a376515f3c2ed53518f19ed16
Sachin <sachin.dhital1998@gmail.com>
2025-09-18 10:40:53 +1000

    Add customer service deployment

--- backend/customer_service/app/main.py
+++ backend/customer_service/app/main.py
@@ -13,7 +13,7 @@ from .db import Base, engine, get_db
 from .models import Customer
 from .schemas import CustomerCreate, CustomerResponse, CustomerUpdate
 
-# --- Standard Logging Configuration ---aaaaa hahahsha aassassa
+# --- Standard Logging Configuration ---aaaaa hahahsha aassassa saaas
 print()
 logging.basicConfig(
     level=logging.INFO,
